e73ef89bac6dc38ad24b65e7cea7dc7b11da0bdb
Elena S <helenserdiuk@gmail.com>
2022-07-25 12:53:42 +0300

    add redux-persist

--- src/redux/initial-state.js
+++ src/redux/initial-state.js
@@ -1,6 +1,11 @@
-const storage = JSON.parse(localStorage.getItem('contacts'));
-
 export const initialState = {
-  items: storage ?? [],
+  items: [],
   filter: '',
 };
+
+// const storage = JSON.parse(localStorage.getItem('contacts'));
+
+// export const initialState = {
+//   items: storage ?? [],
+//   filter: '',
+// };
